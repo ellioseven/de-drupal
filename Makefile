d03995bbb97809c2682cb820f561301e2514634c
Elliot Mitchum <elliot.mitchum@ellioseven.com>
2016-11-01 17:15:49 +1100

    Init

---
+++ Makefile
@@ -0,0 +1,9 @@
+default:
+	@./bin/compile.sh
+	@./bin/push.sh
+
+compile:
+	@./bin/compile.sh
+
+push:
+	@./bin/push.sh
---
+++ dist/apache_2_4_php_5_3_drupal/drupal-install.sh
@@ -0,0 +1,34 @@
+#!/usr/env/bin bash
+
+set -e
+
+DRUPAL_CORE_DEPS="libpng-dev \
+    libjpeg-dev \
+    libmysqlclient-dev"
+
+DRUPAL_CORE_BUILD_DEPS="autoconf \
+    file \
+    g++ \
+    gcc \
+    pkgconf \
+    re2c"
+
+apt-get update \
+    && apt-get -y install --no-install-recommends \
+    $DRUPAL_DEPS \
+    $DRUPAL_BUILD_DEPS \
+    $DRUPAL_CORE_DEPS \
+    $DRUPAL_CORE_BUILD_DEPS
+
+docker-php-ext-get
+docker-php-ext-install gd --with-jpeg-dir --with-png-dir
+docker-php-ext-install opcache
+docker-php-ext-install pdo_mysql
+docker-php-ext-install zip
+docker-php-ext-remove
+
+apt-get -y remove --purge \
+    $DRUPAL_BUILD_DEPS \
+    $DRUPAL_CORE_BUILD_DEPS \
+    && apt-get -y autoremove \
+    && rm -rf /var/lib/apt/lists/
